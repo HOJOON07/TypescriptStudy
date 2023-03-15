4d1ecba4e9cd42272247a6b781c1cc044d6ae308
hojoon <ghwns107@naver.com>
2023-03-16 00:36:22 +0900

    타입 어노테이션

---
+++ test.ts
@@ -0,0 +1,17 @@
+let a: string;
+
+a = "makr"; // O
+
+// a = 39;
+
+// let a = "mark"
+//a는 "Mark" 라는 문자열로 타입이 지정되었기 때문!
+
+// 특정한 변수,개체에 타입을 지정해주는것이 type Annotation
+// :을 붙여주면 된다.
+
+const hello = (b: number) => {
+  //매개변수에도 타입 지정 가능 (어노테이션)
+};
+
+hello("mark"); //문자열 못들어간다.
