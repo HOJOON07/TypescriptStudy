32829d0709063d2a1170bb5176bd62a952b1e5dc
hojoon <ghwns107@naver.com>
2023-04-02 22:49:18 +0900

    class detail

--- Class/01_Example.ts
+++ Class/01_Example.ts
@@ -1,5 +1,13 @@
-class PersonClass {}
+class PersonClass {
+  name;
 
-const classPerson = new PersonClass();
+  //생성자 키워드 constructor
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+//PersonClass라는 이름의 클래스이고 object를 만들었을 때 name이라는 property를 가지고 생성자에 name:string으로 타입이 설정되어있는것.
+
+const classPerson = new PersonClass("mark");
 
 console.log(classPerson);
